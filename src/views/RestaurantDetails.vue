844c341d88ecd7391fd9c2121d226e794d570db3
Luiz <l@pop-os.localdomain>
2022-11-29 17:31:24 +0000

    feat: updated restaurant detail page

--- src/views/RestaurantDetails.vue
+++ src/views/RestaurantDetails.vue
@@ -8,8 +8,9 @@
 				justify-content: center;
 			"
 		>
-			Restaurante
+			Nome do Restaurante
 		</h1>
+
 		<b-carousel
 			id="carousel-1"
 			v-model="slide"
@@ -34,11 +35,74 @@
 				img-src="https://res.cloudinary.com/tf-lab/image/upload/w_600,h_310,c_fill,q_auto,f_auto/restaurant/12fd8c34-59cc-43be-a728-85b6c2f7c416/34f26281-70a0-45db-890c-98ae1d8ed144.png"
 			></b-carousel-slide>
 		</b-carousel>
+		<ul class="mt-5 border border-dark rounded">
+			<li>
+				<font-awesome-icon icon="fa-solid fa-house" color="#502314" /><b>
+					Morada:</b
+				>
+				Praça da Repbública, 123
+			</li>
+			<li>
+				<font-awesome-icon icon="fa-solid fa-file-invoice" color="#502314" /><b>
+					Avaliação:</b
+				>
+				⭐️⭐️⭐️⭐️
+			</li>
+			<li>
+				<font-awesome-icon icon="fa-solid fa-globe" color="#502314" /><b>
+					Website:</b
+				>
+				sitiodorestaurante.com.pt
+			</li>
+			<li>
+				<font-awesome-icon icon="fa-solid fa-phone" color="#502314" /><b>
+					Telemóvel:</b
+				>
+				932-937-475
+			</li>
+			<li>
+				<font-awesome-icon icon="fa-solid fa-clock" color="#502314" /><b>
+					Horário:</b
+				>
+				8:00 –10:00
+			</li>
+		</ul>
+
+		<VueSlickCarousel v-bind="settings">
+			<img src="../assets/coupon.png" alt="Girl in a jacket" width="300" height="140">
+			<img src="../assets/coupon.png" alt="Girl in a jacket" width="300" height="140">
+			<img src="../assets/coupon.png" alt="Girl in a jacket" width="300" height="140">
+			<img src="../assets/coupon.png" alt="Girl in a jacket" width="300" height="140">
+			<img src="../assets/coupon.png" alt="Girl in a jacket" width="300" height="140">
+			<img src="../assets/coupon.png" alt="Girl in a jacket" width="300" height="140">
+			<img src="../assets/coupon.png" alt="Girl in a jacket" width="300" height="140">
+			<img src="../assets/coupon.png" alt="Girl in a jacket" width="300" height="140">
+			<img src="../assets/coupon.png" alt="Girl in a jacket" width="300" height="140">
+			<img src="../assets/coupon.png" alt="Girl in a jacket" width="300" height="140">
+			<img src="../assets/coupon.png" alt="Girl in a jacket" width="300" height="140">
+			<img src="../assets/coupon.png" alt="Girl in a jacket" width="300" height="140">
+		</VueSlickCarousel>
 	</div>
 </template>
 
 <script>
+	import VueSlickCarousel from 'vue-slick-carousel';
+	import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+
+	import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
 	export default {
 		name: 'RestaurantDetails',
+		components: { VueSlickCarousel },
+		data() {
+			return {
+				settings: {
+					infinite: true,
+					slidesToShow: 3,
+					speed: 500,
+					rows:2,
+					slidesPerRow: 1,
+				},
+			};
+		},
 	};
 </script>
